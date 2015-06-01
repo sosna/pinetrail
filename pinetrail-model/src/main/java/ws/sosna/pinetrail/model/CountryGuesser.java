a8cfdf3d4a3b419b94ccb6c954994a60df1a3b47
Xavier Sosnovsky <xso@sosna.ws>
2015-06-01 20:26:31 +0200

    Added reverse geocoding for multiple points

--- pinetrail-model/src/main/java/ws/sosna/pinetrail/model/CountryGuesser.java
+++ pinetrail-model/src/main/java/ws/sosna/pinetrail/model/CountryGuesser.java
@@ -20,12 +20,16 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.function.Function;
+import java.util.prefs.Preferences;
+import java.util.stream.Collectors;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -48,13 +52,9 @@ import ws.sosna.pinetrail.utils.logging.StatusCodes;
  * This is possible thanks to the reverse geocoding facility kindly offered by
  * mapquest (http://open.mapquestapi.com/nominatim/).
  *
- * <p>
- * By default, the class will randomly select one point for geocoding but other
- * strategies could be used later, if needed (e.g. select one point per hour).
- *
  * @author Xavier Sosnovsky
  */
-enum CountryGuesser implements Function<Set<Waypoint>, Set<String>> {
+enum CountryGuesser implements Function<SortedSet<Waypoint>, Set<String>> {
 
     /**
      * Singleton that returns an instance of a CountryGuesser.
@@ -62,7 +62,7 @@ enum CountryGuesser implements Function<Set<Waypoint>, Set<String>> {
     INSTANCE;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(
-            CountryGuesser.class);
+        CountryGuesser.class);
     private static final int DEFAULT_TIME_OUT = 3000;
 
     /**
@@ -72,18 +72,19 @@ enum CountryGuesser implements Function<Set<Waypoint>, Set<String>> {
      * @return the countries crossed by the trail
      */
     @Override
-    public Set<String> apply(final Set<Waypoint> points) {
-        final Waypoint selected = getSelectedPoint(points);
+    public Set<String> apply(final SortedSet<Waypoint> points) {
         final Set<String> countries = new LinkedHashSet<>();
         try {
-            final String countryCode =
-                getCountry(parseXml(askMapquest(selected))).toUpperCase();
-            countries.add(countryCode);
+            countries.addAll(getSelectedPoints(points).parallelStream()
+                .map(p -> getCountry(parseXml(askMapquest(p))))
+                .map(String::toUpperCase)
+                .collect(Collectors.toSet()));
             return countries;
         } catch (final ExecutionError e) {
             LOGGER.warn(e.getMarker(), "{} | {} | {}", e.getAction(),
-            e.getErrorCode().getCode(), e.getMessage()
-            + (null == e.getCause() ? "" : ": " + e.getCause().getMessage()));
+                e.getErrorCode().getCode(), e.getMessage()
+                + (null == e.getCause() ? "" : ": "
+                    + e.getCause().getMessage()));
             return countries;
         }
     }
@@ -93,9 +94,44 @@ enum CountryGuesser implements Function<Set<Waypoint>, Set<String>> {
      * collection of waypoints. The default strategy is to randomely select one
      * in the collection of points.
      */
-    private Waypoint getSelectedPoint(final Set<Waypoint> points) {
+    private Set<Waypoint> getSelectedPoints(final SortedSet<Waypoint> points) {
+        final Set<Waypoint> selected = new LinkedHashSet<>();
+        if (Boolean.parseBoolean(Preferences.userRoot()
+            .node("ws.sosna.pinetrail.model.Trail")
+            .get("crossBorder", "false"))) {
+            selected.addAll(getOnePointPerHour(points));
+        } else {
+            selected.add(getRandomPoint(points));
+        }
+        return selected;
+    }
+
+    /*
+     * Returns the point selected randomely for reverse geocoding out of the
+     * supplied collection of waypoints.
+     */
+    private Waypoint getRandomPoint(final SortedSet<Waypoint> points) {
         final List<Waypoint> ptsList = new ArrayList<>(points);
         return ptsList.get(new Random().nextInt(points.size()));
+    }
+
+    /*
+     * Returns the points selected randomely for reverse geocoding out of the
+     * supplied collection of waypoints, taking one point per hour.
+     */
+    private Set<Waypoint> getOnePointPerHour(final SortedSet<Waypoint> points) {
+        final Set<Waypoint> selected = new LinkedHashSet<>();
+        final long hours = ChronoUnit.HOURS.between(points.first().getTime(),
+            points.last().getTime());
+        final double step = Math.floor(points.size() / hours);
+        int count = 0;
+        final ArrayList<Waypoint> ptsList = new ArrayList<>(points);
+        while (count < points.size()) {
+            selected.add(ptsList.get(count));
+            count += step;
+        }
+        selected.add(points.last());
+        return selected;
     }
 
     /*
@@ -110,10 +146,10 @@ enum CountryGuesser implements Function<Set<Waypoint>, Set<String>> {
         try {
             final URL target = new URL(url);
             LOGGER.debug(Markers.NETWORK.getMarker(), "{} | {} | Created URL "
-                    + "for reverse geocoding: {}", Actions.CREATE,
-                    StatusCodes.OK.getCode(), url);
+                + "for reverse geocoding: {}", Actions.CREATE,
+                StatusCodes.OK.getCode(), url);
             final HttpURLConnection conn
-                    = (HttpURLConnection) target.openConnection();
+                = (HttpURLConnection) target.openConnection();
             //Abort after 3 seconds
             conn.setConnectTimeout(DEFAULT_TIME_OUT);
             conn.connect();
@@ -133,17 +169,18 @@ enum CountryGuesser implements Function<Set<Waypoint>, Set<String>> {
      */
     private Document parseXml(final InputStream xmlInput) {
         final DocumentBuilderFactory dbFactory
-                = DocumentBuilderFactory.newInstance();
+            = DocumentBuilderFactory.newInstance();
         try {
             final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             final Document doc = dBuilder.parse(xmlInput);
             LOGGER.debug(Markers.MODEL.getMarker(), "{} | {} | Mapquest "
-                    + "response successfully read as DOM object.",
-                    Actions.PARSE, StatusCodes.OK.getCode());
+                + "response successfully read as DOM object.",
+                Actions.PARSE, StatusCodes.OK.getCode());
             return doc;
         } catch (final ParserConfigurationException e) {
-            throw new ExecutionError("Error creating DOM builder", e, Markers.
-                MODEL.getMarker(), Actions.CREATE, StatusCodes.INTERNAL_ERROR);
+            throw new ExecutionError("Error creating DOM builder", e,
+                Markers.MODEL.getMarker(), Actions.CREATE,
+                StatusCodes.INTERNAL_ERROR);
         } catch (final SAXException e) {
             throw new ExecutionError("Error parsing XML from mapquest (SAX)", e,
                 Markers.MODEL.getMarker(), Actions.PARSE,
@@ -161,23 +198,23 @@ enum CountryGuesser implements Function<Set<Waypoint>, Set<String>> {
     private String getCountry(final Document xmlDocument) {
         String country = null;
         final NodeList nList
-                = xmlDocument.getElementsByTagName("addressparts");
+            = xmlDocument.getElementsByTagName("addressparts");
         for (int temp = 0; temp < nList.getLength(); temp++) {
             final Node nNode = nList.item(temp);
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                 final Element eElement = (Element) nNode;
                 country = eElement.getElementsByTagName("country_code").
-                        item(0).getTextContent();
+                    item(0).getTextContent();
                 LOGGER.debug(Markers.MODEL.getMarker(), "{} | {} | Mapquest"
-                        + " response contains a country code: {}.",
-                        Actions.ANALYSE, StatusCodes.OK.getCode(), country);
+                    + " response contains a country code: {}.",
+                    Actions.ANALYSE, StatusCodes.OK.getCode(), country);
                 break;
             }
         }
         if (null == country) {
             throw new ExecutionError("Could not find a country code in the"
-                    + " response from Mapquest.", null, Markers.MODEL.
-                    getMarker(), Actions.GET, StatusCodes.NOT_FOUND);
+                + " response from Mapquest.", null, Markers.MODEL.
+                getMarker(), Actions.GET, StatusCodes.NOT_FOUND);
         }
         return country;
     }
