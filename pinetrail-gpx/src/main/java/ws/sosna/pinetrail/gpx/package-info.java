827e8223f03f9f7c9eddaad0a2a640da47253451
Xavier Sosnovsky <xso@sosna.ws>
2020-09-06 20:36:14 +0200

    Update docs

--- pinetrail-gpx/src/main/java/ws/sosna/pinetrail/gpx/package-info.java
+++ pinetrail-gpx/src/main/java/ws/sosna/pinetrail/gpx/package-info.java
@@ -1,9 +1,9 @@
 /**
  * Provides thread-safe readers that extract information from GPX data files.
  *
- * <p>{@code Gpx11ReaderProvider} acts as a service provider, as defined in Java SPI, and will be
- * automatically registered with the simple service-provider loading facility offered by the {@code
- * Readers} class.
+ * <p>{@code Gpx11Provider} and {@code Gpx10Provider} act as service providers, as defined in Java
+ * SPI, and will be automatically registered with the service-provider loading facility offered by
+ * the {@code Readers} class.
  *
  * @author Xavier Sosnovsky
  * @see ws.sosna.pinetrail.api.io.Readers
