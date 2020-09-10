2eaf86422c9f1d36b3654b3d56e9c3399f8475ec
Xavier Sosnovsky <xso@sosna.ws>
2020-09-10 21:18:51 +0200

    Code cleanup

--- pinetrail-utils/src/test/java/ws/sosna/pinetrail/utils/logging/MarkersTest.java
+++ pinetrail-utils/src/test/java/ws/sosna/pinetrail/utils/logging/MarkersTest.java
@@ -15,26 +15,26 @@
  */
 package ws.sosna.pinetrail.utils.logging;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.slf4j.Marker;
 
-/**
- * @author Xavier Sosnovsky
- */
+/** @author Xavier Sosnovsky */
 public class MarkersTest {
 
-    @Test
-    public void getMarker() {
-        final Markers audit =  Markers.valueOf("DB");
-        final Marker marker = audit.getMarker();
-        assertEquals("db", marker.getName());
-    }
+  @Test
+  public void getMarker() {
+    final Markers audit = Markers.valueOf("DB");
+    final Marker marker = audit.getMarker();
+    assertEquals("db", marker.getName());
+  }
 
-    @Test
-    public void sameInstance() {
-        final Marker marker1 = Markers.MODEL.getMarker();
-        final Marker marker2 = Markers.MODEL.getMarker();
-        assertSame(marker1, marker2);
-    }
+  @Test
+  public void sameInstance() {
+    final Marker marker1 = Markers.MODEL.getMarker();
+    final Marker marker2 = Markers.MODEL.getMarker();
+    assertSame(marker1, marker2);
+  }
 }
